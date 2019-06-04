5a35bdd4a63749b6755f6b5a3b983f98ab2970dc
wxhccc <wxhccche@sina.com>
2019-06-04 13:18:54 +0800

    fix bug

--- src/object-array.js
+++ src/object-array.js
@@ -6,7 +6,7 @@ export function mapToObject (objectArray, keyProp = 'key', valueProp = 'value') 
   && objectArray.forEach(item => {
     const key = typeof keyProp === 'string' ? item[keyProp] : keyProp(item)
     const value = typeof valueProp === 'string' ? item[valueProp] : valueProp(item)
-    key && ['string', 'number'].includes(typeof key) && (result[key] = value)
+    ;['string', 'number'].includes(typeof key) && (result[key] = value)
   })
   return result
 }
